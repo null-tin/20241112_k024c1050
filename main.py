46355597eaafcd43a24ba3d17684c3a907d5ba43
null <k024c1050@g.neec.ac.jp>
2024-11-12 07:06:51 +0000

    [update]ユースケ版に変更
    [add] style.css, card-age.html, app_keiji.py, app_yuusuke.pu
    [remove] card.html

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Flaskのインスタンスを作成 --- (*1)
 app = Flask(__name__)
 
-# ルーティングの指定 --- (*2)
 @app.route('/')
 def index():
-    return "Hello, World!"
+    # データを指定 --- (*1)
+    username = 'ケイジ'
+    age = 19
+    email = 'keiji@example.com'
+    # テンプレートエンジンにデータを指定 --- (*2)
+    return render_template('card.html',
+            username=username,
+            age=age,
+            email=email)
 
-# 実行する --- (*3)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
